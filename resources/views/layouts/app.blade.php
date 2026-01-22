a40f101e90624d49e78d4d647c777166af1de1da
wahid-git200 <abdullwahidnajafi@gmail.com>
2026-01-22 11:16:39 +0430

    car folder that contains all car CRUDS process

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,4 @@
 
     {{$slot}}
       
-    
-    <footer>
-        <a href="#">link 1</a>
-        <a href="#">link 2</a>
-        {{$footerLinks}}
-    </footer>
-
 </x-base-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarControler;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\signinController;
 use App\Http\Controllers\signupController;
@@ -7,6 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[homeController::class,'index'])->name('home');
 
+
+Route::get('/car/search',[CarControler::class,'search'])->name('car.search');
+Route::resource('car',CarControler::class); 
 
 Route::get('/singin',[signinController::class,'create'])->name('singin');
 Route::get('/singup',[signupController::class,'create'])->name('singup');
